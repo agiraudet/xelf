76e7292501d202b91f9da75d30a022732aef94f1
agiraude <agiraude@student.42.fr>
2023-12-13 22:22:59 +0100

    Woody mostly working

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include "cypher.h"
+#include "inject.h"
+#include "segc.h"
+#include "xelf.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <elf_file\n", argv[0]);
+    return 1;
+  }
+  struct xelf *xelf;
+  struct cypher *cypher;
+
+  xelf = xelf_create(argv[1]);
+  if (!xelf)
+    return 1;
+  cypher = cypher_create();
+  if (!cypher) {
+    xelf_close(xelf);
+    return 2;
+  }
+  Elf64_Shdr *text_sec = sec_find_by_name(xelf, ".text");
+  sec_encrypt_xor(xelf, text_sec, cypher);
+  Elf64_Phdr *code_seg = seg_find_by_charac(xelf, PT_LOAD, (PF_R | PF_X));
+  if (!code_seg) {
+    fprintf(stderr, "Could not find a code segment !\n");
+    xelf_close(xelf);
+    return 3;
+  }
+  seg_set_flags(code_seg, (PF_R | PF_X | PF_W));
+  inject_cypher(xelf, code_seg, cypher);
+  xelf_close(xelf);
+  // TODO clean memory leaks
+  return 0;
+}
--- src/xelf.c
+++ src/xelf.c
@@ -37,24 +37,6 @@ int xelf_open(struct xelf *xelf, const char *path) {
 
 int xelf_close(struct xelf *xelf) { return munmap(xelf->elf, xelf->size); }
 
-Elf64_Phdr *xelf_find_seg_by_charac(struct xelf *xelf, uint32_t type,
-                                    uint32_t flags) {
-  if (!xelf)
-    return 0;
-  for (unsigned int i = 0; i < xelf->header->e_phnum; i++) {
-    Elf64_Phdr *seg = &xelf->prog_header_tab[i];
-    if (seg->p_type == type && seg->p_flags == flags) {
-      return seg;
-    }
-  }
-  return 0;
-}
-
-void xelf_seg_set_flags(Elf64_Phdr *seg, uint32_t flags) {
-  if (seg)
-    seg->p_flags = flags;
-}
-
 struct xelf *xelf_create(const char *path) {
   struct xelf *xelf;
   xelf = malloc(sizeof(struct xelf));
@@ -78,35 +60,4 @@ struct xelf *xelf_create(const char *path) {
     return 0;
   }
   return xelf;
-}
-
-int xelf_pack(const char *path) {
-  struct xelf *xelf;
-  struct cypher *cypher;
-  struct inject inject;
-
-  xelf = xelf_create(path);
-  if (!xelf)
-    return 1;
-  cypher = xelf_cypher_create();
-  if (!cypher) {
-    xelf_close(xelf);
-    return 2;
-  }
-  Elf64_Shdr *text_sec = xelf_find_sec_by_name(xelf, ".text");
-  xelf_sec_encrypt_xor(xelf, text_sec, cypher);
-  Elf64_Phdr *code_seg = xelf_find_seg_by_charac(xelf, PT_LOAD, (PF_R | PF_X));
-  if (!code_seg) {
-    fprintf(stderr, "Could not find a code segment !\n");
-    xelf_close(xelf);
-    return 3;
-  }
-  xelf_seg_set_flags(code_seg, (PF_R | PF_X | PF_W));
-
-  // TODO replace with headers, not from file
-  if (xelf->header->e_type == ET_DYN)
-    xelf_inject_load_from_file(&inject, "so_payload");
-  else if (xelf->header->e_type == ET_EXEC)
-    xelf_inject_load_from_file(&inject, "payload");
-  ///////
 }
